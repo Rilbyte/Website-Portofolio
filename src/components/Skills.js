b017bb9be6f17c2303c04fea8b2c437537fe435e
Rilbyte <arielprtm2222@gmail.com>
2026-03-12 10:52:17 +0700

    fix skills alt text and remove unused import

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,63 +4,61 @@ import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
-import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
-      return(
-        <section className="skills" id="skills">
-            <Container>
-                <Row>
-                    <Col>
-                    <div className="skill-bx">
-                        <h2>
-                            SOFT SKILLS
-                        </h2>
-                        <p>
-                            <br></br> 
-                        </p>
-                        <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt='Image'></img>
-                                <h5>Communication</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt='Image'></img>
-                                <h5>Problem-solving</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt='Image'></img>
-                                <h5>Time management</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt='Image'></img>
-                                <h5>Creativity</h5>
-                            </div>
-                        </Carousel>
-                    </div>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-      )
-}
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
+  return (
+    <section className="skills" id="skills">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>SOFT SKILLS</h2>
+
+              <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                <div className="item">
+                  <img src={meter1} alt="Communication skill level" />
+                  <h5>Communication</h5>
+                </div>
+
+                <div className="item">
+                  <img src={meter2} alt="Problem solving skill level" />
+                  <h5>Problem-solving</h5>
+                </div>
+
+                <div className="item">
+                  <img src={meter3} alt="Time management skill level" />
+                  <h5>Time management</h5>
+                </div>
+
+                <div className="item">
+                  <img src={meter1} alt="Creativity skill level" />
+                  <h5>Creativity</h5>
+                </div>
+              </Carousel>
+
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
